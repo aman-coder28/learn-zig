1f838ea2524e5341333190ba60fb4c938cade672
aman-coder28 <amancoder28@gmail.com>
2026-07-13 15:49:19 +0300

    reorder files

--- src/main.zig
+++ src/main.zig
@@ -1,54 +1,8 @@
 const std = @import("std");
-const divide = @import("root.zig").divide;
+const calc = @import("calc.zig").calc;
 
 const print = std.debug.print;
 
 pub fn main(input: std.process.Init) !void {
-    var fnum_buf: [1024]u8 = undefined;
-    const first = readline_to_float(input, &fnum_buf, "Enter the first Number: ") catch 0;
-
-    var opr_buf: [1024]u8 = undefined;
-    const opr = readline(input, &opr_buf, "Enter an Operator: ") catch " ";
-
-    var snum_buf: [1024]u8 = undefined;
-    const second = readline_to_float(input, &snum_buf, "Enter the second Number: ") catch 0;
-
-    calc(first, opr[0], second);
-}
-
-fn readline(init: std.process.Init, buf: []u8, text: []const u8) ![]const u8 {
-    print("{s}", .{text});
-    var stdin_reader = std.Io.File.stdin().reader(init.io, buf);
-    const reader = &stdin_reader.interface;
-
-    const line = try reader.takeDelimiter('\n') orelse unreachable;
-
-    return std.mem.trim(u8, line, "\r");
-}
-
-fn readline_to_float(init: std.process.Init, buf: []u8, text: []const u8) !f32 {
-    const line = readline(init, buf, text) catch "";
-
-    return std.fmt.parseFloat(f32, line) catch 0;
-}
-
-fn calc(
-    first: f32,
-    opr: u8,
-    second: f32,
-) void {
-    switch (opr) {
-        '+' => calc_print(first, opr, second, first + second),
-        '-' => calc_print(first, opr, second, first - second),
-        'x' => calc_print(first, opr, second, first * second),
-        '/' => calc_print(first, opr, second, divide(first, second) catch |err| switch (err) {
-            error.NoDivisionByZero => 0.0,
-            error.XCantBeZero => 1.0,
-        }),
-        else => print("\n please enter a valid operator (+, -, x, /).", .{}),
-    }
-}
-
-fn calc_print(first: f32, opr: u8, second: f32, res: f32) void {
-    print("\n{d} {c} {d} = {d}", .{ first, opr, second, res });
+    calc(input);
 }
