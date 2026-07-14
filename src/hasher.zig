1add9b982c294d25bfa33b4f1cd77e27459077a2
aman-coder28 <amancoder28@gmail.com>
2026-07-15 01:33:57 +0300

    try some encryption stuff

---
+++ src/hasher.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn hash(word: []const u8, key: []const u8) ![32]u8 {
+    var out: [32]u8 = undefined;
+
+    std.crypto.auth.hmac.sha2.HmacSha256.create(&out, word, key);
+
+    return out;
+}
+
+pub fn encrypt(allocator: std.mem.Allocator, plaintext: []const u8, key: [32]u8, nonce: [12]u8) ![]u8 {
+    const Aes256Gcm = std.crypto.aead.aes_gcm.Aes256Gcm;
+
+    var out = try allocator.alloc(u8, plaintext.len + Aes256Gcm.tag_length);
+    errdefer allocator.free(out);
+
+    const ciphertext = out[0..plaintext.len];
+    var tag: [Aes256Gcm.tag_length]u8 = undefined;
+
+    Aes256Gcm.encrypt(ciphertext, &tag, plaintext, &[_]u8{}, nonce, key);
+
+    @memcpy(out[plaintext.len..], &tag);
+
+    return out;
+}
+pub fn decrypt(allocator: std.mem.Allocator, ciphertext_with_tag: []const u8, key: [32]u8, nonce: [12]u8) ![]u8 {
+    const Aes256Gcm = std.crypto.aead.aes_gcm.Aes256Gcm;
+
+    if (ciphertext_with_tag.len < Aes256Gcm.tag_length) return error.InvalidInput;
+
+    const ciphertext = ciphertext_with_tag[0 .. ciphertext_with_tag.len - Aes256Gcm.tag_length];
+
+    var tag: [Aes256Gcm.tag_length]u8 = undefined;
+    @memcpy(&tag, ciphertext_with_tag[ciphertext_with_tag.len - Aes256Gcm.tag_length ..]);
+
+    const plaintext = try allocator.alloc(u8, ciphertext.len);
+    errdefer allocator.free(plaintext);
+
+    try Aes256Gcm.decrypt(plaintext, ciphertext, tag, &[_]u8{}, nonce, key);
+
+    return plaintext;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,33 @@
 const std = @import("std");
-const calc = @import("calc.zig").calc;
 const cap = @import("cap.zig");
+const hasher = @import("hasher.zig");
 const print = std.debug.print;
 const heap = std.heap;
 
-// init: std.process.Init
 pub fn main() !void {
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
 
-    // calc(init);
-
     const allocator = arena.allocator();
-    const text = "hello world";
 
-    print("{s}", .{try cap.capper(text, allocator)});
+    const text = "hi, hello world?";
+    const key = "aa55b3794e674ccf8a3758021c3d7284";
+
+    const encrypted = try hasher.encrypt(
+        allocator,
+        text,
+        @constCast(key).*,
+        @constCast("8af80924f4ab").*,
+    );
+
+    print("{x} \n", .{encrypted});
+
+    print("{s} \n", .{try hasher.decrypt(
+        allocator,
+        encrypted,
+        @constCast(key).*,
+        @constCast("8af80924f4ab").*,
+    )});
+
+    // print("{s}", .{try cap.capper(text, allocator)});
 }
