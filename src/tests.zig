488effe813063253a949feee265092fa201fb255
aman-coder28 <amancoder28@gmail.com>
2026-07-19 22:28:32 +0300

    Update tests.zig

--- src/tests.zig
+++ src/tests.zig
@@ -1,26 +1,33 @@
 const std = @import("std");
 const hasher = @import("./hasher.zig");
-const testing = std.testing;
+const allocator = std.testing.allocator;
 const io = std.testing.io;
 
 test "encryption and decryption" {
     const encrypted = try hasher.encryptWithPassword(
-        testing.allocator,
+        allocator,
         io,
         "text",
         "password",
     );
 
+    const cipher = "3324082eed31be2863d96c4abd9754606bd3023c1ecec4b870ba20448c9896b2d1cf2033617b93fe4cdaa110ef4a28da";
+
+    const buffer = try allocator.alloc(u8, cipher.len);
+    defer allocator.free(buffer);
+
+    const bytes = try std.fmt.hexToBytes(buffer, cipher);
+
     const decrypted = try hasher.decryptWithPassword(
-        testing.allocator,
+        allocator,
         io,
-        encrypted,
+        bytes,
         "password",
     );
 
     defer {
-        _ = testing.allocator.free(encrypted);
-        _ = testing.allocator.free(decrypted);
+        _ = allocator.free(encrypted);
+        _ = allocator.free(decrypted);
     }
 
     try std.testing.expectEqualStrings(
